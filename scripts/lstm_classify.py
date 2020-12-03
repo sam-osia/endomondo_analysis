63ea0d5c0f1a48c20160ac899a15c00c3c949e0a
sam-osia <sam.osia95@gmail.com>
2020-12-02 21:06:36 -0500

    started model creation

---
+++ scripts/lstm_classify.py
@@ -0,0 +1,40 @@
+import tensorflow as tf
+import tensorflow.keras as keras
+from tensorflow.keras.models import Sequential, Model, load_model, save_model
+from tensorflow.keras.layers import Dense, Lambda, Activation
+from tensorflow.keras.layers import Embedding, Input, Dense, Reshape, Flatten, MaxPooling1D, Dropout, LSTM
+
+from sklearn import preprocessing
+
+import numpy as np
+import pandas as pd
+import os
+
+from utils import *
+
+
+def create_model():
+    num_users = 51
+    num_steps = 300
+    model = Sequential()
+    model.add(Input(shape=(num_steps, 1)))
+    model.add(Embedding(input_dim=51, output_dim=5))
+    # model.add(Lambda(lambda y: tf.squeeze(y, 2)))
+
+    print(model.summary())
+    return model
+
+
+set_path('saman')
+
+df = pd.read_json('./data/data_chunk.json')
+le = preprocessing.LabelEncoder()
+le.fit(df['userId'])
+df['userCat'] = le.transform(df['userId'])
+print(df['userCat'])
+
+
+model = create_model()
+input = np.array(df['userCat']).astype('int64')
+shape_var = model(tf.convert_to_tensor(input))
+print(shape_var.shape)
--- scripts/utils.py
+++ scripts/utils.py
@@ -4,7 +4,16 @@ import pandas as pd
 import os
 import pickle
 import json
+import sys
 
+
+def set_path(user):
+    if user == 'sayeh':
+        os.chdir(os.path.dirname(sys.argv[0]))
+    elif user == 'kasra':
+        os.chdir(os.chdir(os.path.dirname(sys.argv[0])))
+
+    os.chdir('..')
 
 def pkl_load(file_path):
     with open(file_path, 'rb') as f:
@@ -19,20 +28,27 @@ def io_load(file_path):
             print(line)
             break
 
+
 def pd_load(file_path):
     df = pd.read_pickle(file_path)
     print(df.shape)
     print(df.head())
 
 
-def json_load():
+def create_chunk(rows=5000):
     # Since the dataset downloaded is in json format, apply the following function to open the file, then transform it into a csv file.
     reviews = []
     with open('./data/processed_endomondoHR_proper_interpolate.json', 'r') as train_file:
-        for l in train_file:
+        for i, l in enumerate(train_file):
             reviews.append(json.loads(l.strip()))
+            if i > rows:
+                break
 
+    df = pd.DataFrame.from_dict(reviews)
+    df.to_json('./data/data_chunk.json')
 
-os.chdir('..')
-file_path = './data/endomondoHR_proper_metaData.pkl'
-json_load()
+    return df
+
+if __name__ == '__main__':
+    os.chdir('..')
+    create_chunk()
