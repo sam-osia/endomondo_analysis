83703c70f146322b2550a707d23cd39e8d33e8ee
sam-osia <sam.osia95@gmail.com>
2020-12-04 12:50:45 -0500

    made logger log to command line, output needs to be piped into file instead

--- scripts/slurm_test.py
+++ scripts/slurm_test.py
@@ -9,15 +9,14 @@ set_path('saman')
 Path('./logs/test_logs').mkdir(parents=True, exist_ok=True)
 
 log_dir = './logs/test_logs'
-logging.basicConfig(filename=os.path.join(log_dir, 'test.log'),
-                    filemode='a',
-                    format='%(asctime)s - %(message)s',
+logging.basicConfig(format='%(asctime)s - %(message)s',
                     datefmt='%d-%b-%y %H:%M:%S',
                     level=logging.INFO)
 
 logger = logging.getLogger('slurm_log ')
 
 logger.info('hello from remote')
+logger.info(f'Version: {tf.__version__}')
 logger.info(f'Built with Cuda - {tf.test.is_built_with_cuda()}')
-# logging.info(f'GPU count - {len(tf.config.list_physical_devices("GPU"))}')
+logging.info(f'GPU count - {len(tf.config.list_physical_devices("GPU"))}')
 
