4a4b0c4b098039b324e91dcfa2624cc31b23aaa4
SayehBayat <sayeh.bayat@mail.utoronto.ca>
2020-12-04 19:23:43 -0500

    Created a test preprocess for female_bike.json with distance, altitude, and time_elapsed

--- data/preprocess.py
+++ data/preprocess.py
@@ -5,22 +5,23 @@ import pandas as pd
 import numpy as np
 import itertools
 
+set_path("sayeh")
 
 def create_time_series_data(df, targetDim):
     df = df.reset_index(drop=True)
     nRows = len(df)
     print(nRows)
-    s = (nRows,targetDim*300)
+    s = (nRows, targetDim*300)
     data = np.zeros(s)
     targData = np.zeros((nRows, 300))
     for i in range(nRows):
         data[i, :] = list(itertools.chain(df["time_elapsed"][i], df["distance"][i], df["altitude"][i]))
-        targData[i,:] = df["heart_rate"][i]
+        targData[i, :] = df["heart_rate"][i]
     return data, targData
 
 if __name__ == "__main__":
     df = pd.read_json('/Users/sayehbayat/Downloads/female_bike.json')
     targetDim = 3
     formatted_data, targData = create_time_series_data(df, targetDim*300)
-    print(formatted_data.shape)
-    print(targData.shape)
+    print("Input data shape:", formatted_data.shape)
+    print("Output data shape:", targData.shape)
