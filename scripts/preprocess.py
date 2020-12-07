ad5b45767890b0f7709af92e6a0a678c4b59307f
SayehBayat <sayeh.bayat@mail.utoronto.ca>
2020-12-07 15:53:46 -0500

    Categorical feature formatting

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 import itertools
 from utils import *
+from sklearn import preprocessing
 
 
 
@@ -19,11 +20,34 @@ def create_time_series_data(df, targetDim):
         targData[i, :, 0] = df["heart_rate"][i]
     return data, targData
 
+def process_catData(df, feature):
+
+    df = df.reset_index(drop=True)
+    le = preprocessing.LabelEncoder()
+    data = np.zeros((len(df), 1, 1))
+    le.fit(df[feature])
+    data[:, 0, 0] = le.transform(df[feature])
+
+    return len(np.unique(df["userId"])), data
 if __name__ == "__main__":
     set_path("sayeh")
     df = pd.read_json('./data/female_bike.json')
-    print(df.head())
+
+    data = []
+    #with open('./data/processed_endomondoHR_proper_interpolate.json', 'r') as train_file:
+    #    for l in train_file:
+    #        data.append(json.loads(l.strip()))
+    #df = pd.DataFrame.from_dict(data)
+    print(df.columns)
     targetDim = 3
     formatted_data, targData = create_time_series_data(df, targetDim)
     print("Input data shape:", formatted_data.shape)
     print("Output data shape:", targData.shape)
+
+    nUser, formatted_gender = process_catData(df, 'gender')
+    print("Number of Users:", nUser)
+    print("Transformed Gender:", formatted_gender)
+
+    nUser, formatted_sport = process_catData(df, 'sport')
+    print("Number of Users:", nUser)
+    print("Transformed Sport:", np.unique(formatted_sport ))
