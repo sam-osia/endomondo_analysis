012b01ef6152f07ad66af28440848521cb7f5d3e
sam-osia <sam.osia95@gmail.com>
2020-12-09 01:42:42 -0500

    even more complicated

--- scripts/even_more_complicated_model.py
+++ scripts/even_more_complicated_model.py
@@ -103,21 +103,62 @@ class EvenMoreComplicatedModel(BaseModel):
         return hyperparams
 
 
-
-
 if __name__ == '__main__':
     set_path('saman')
-    data_paths = ['./data/male_run.json',
-                  './data/female_run.json',
-                  './data/male_bike.json',
-                  './data/female_bike.json']
+    # data_paths = ['./data/male_run.json',
+    #               './data/female_run.json',
+    #               './data/male_bike.json',
+    #               './data/female_bike.json']
+    #
+    # model = EvenMoreComplicatedModel(run_id=-1,
+    #                              df_paths=data_paths,
+    #                              generate_hyperparams=False,
+    #                              testing=True)
+    # model.run_pipeline()
 
-    data_paths = './data/female_bike.json'
 
-    model = EvenMoreComplicatedModel(run_id=-1,
-                                 df_paths=data_paths,
-                                 generate_hyperparams=False,
-                                 testing=True)
-    model.run_pipeline()
+    data_names = ['male_run',
+                  'female_run',
+                  'male_bike',
+                  'female_bike']
 
+    df = None
+    for data_name in data_names:
+        print(f'./data/{data_name}.json')
+        df_temp = pd.read_json(f'./data/{data_name}.json')
+        if 'female' not in data_name:
+            df_temp = df_temp.sample(frac=0.5)
+        if df is None:
+            df = df_temp
+        else:
+            df = pd.concat([df, df_temp])
+
+    model = keras.models.load_model(
+        './models/even_more_complicated/model_weights/even_more_complicated_2020_12_09-01_04_11.h5')
+
+    input_speed, input_alt, input_gender, input_sport, input_user, input_time_last, prevData, targData \
+        = curr_preprocess(df)
+
+    input_temporal = np.dstack([input_speed, input_alt])
+
+    idx_list = np.random.randint(1, 20000, 12)
+
+    for plot_ind, i in enumerate(idx_list):
+        inputs = [input_sport[i].reshape(1, 300, 1),
+                  input_gender[i].reshape(1, 300, 1),
+                  input_temporal[i].reshape(1, 300, 2),
+                  prevData[i].reshape(1, 300, 3)]
+
+        sport = input_sport[i][0]
+        gender = input_gender[i][0]
+
+        pred = model.predict(inputs).reshape(-1)
+        actual = targData[i]
+        plt.subplot(3, 4, plot_ind + 1)
+        plt.plot(actual, color='r', label='actual')
+        plt.plot(pred, color='b', label='pred')
+        plt.title(f'Gender: {gender}, Sport: {sport}')
+
+    plt.legend()
+    plt.show()
 
